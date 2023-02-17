9f70b784657d5de5d47e4f5b29c3018c125b44c9
MozartOnWeb <mozartontheweb@gmail.com>
2023-02-17 02:00:06 +0000

    working on categories section

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
+
 import styles from "./categories.module.css";
+
+import HeroImage from "@/public/images/1.jpg";
 
 export const metadata = {
   title: "Catégories",
@@ -6,5 +10,27 @@ export const metadata = {
 };
 
 export default function Categories() {
-  return <main className={styles.main}>Categories</main>;
+  return (
+    <main className={styles.main}>
+      {/* HERO SECTION */}
+      <section className={styles.hero}>
+        <div>
+          <Image src={HeroImage} alt="dagaba catégories image" />
+        </div>
+        <h3>
+          Pharmacopée Dagaba, chez <span>Mouleikafouf</span>.
+        </h3>
+        <span className={styles.numberOfStock}>+2000 remèdes</span>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+      </section>
+
+      {/* CATEGORIES SECTION */}
+      <section className={styles.categories}></section>
+    </main>
+  );
 }
