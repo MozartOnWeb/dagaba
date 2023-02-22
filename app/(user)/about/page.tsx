0f6b694d559e15abf406ba1bb5b216d81478518c
MozartOnWeb <mozartontheweb@gmail.com>
2023-02-22 00:56:43 +0000

    working on about page

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
+
+import styles from "./styles.module.css";
+
+import img from "@/public/images/4.jpg";
+
 export const metadata = {
   title: "A Propos",
   description: "Ce qu'il faut savoir sur DAGABA",
 };
 
 export default function About() {
-  return <div>about</div>;
+  return (
+    <main className={styles.main}>
+      <section className={styles.hero}>
+        <div>
+          <h2>
+            La santé, la propriété, la paix, le développement ont toujours été
+            l’objectif constamment visé par APROPHAM DAGABA
+          </h2>
+          <div></div>
+        </div>
+        <Image src={img} alt="hero" />
+      </section>
+    </main>
+  );
 }
