78758cd59dd9bd5db0dab0f8b48261a53430d451
MozartOnWeb <mozartontheweb@gmail.com>
2023-04-15 13:30:35 +0000

    add openGraph image

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -6,11 +6,30 @@ import Footer from "@/components/footer/Footer";
 import { montserrat, clashDisplay } from "@/public/fonts";
 
 export const metadata = {
+  metadataBase: new URL("https://dagaba.net"),
   title: {
     default: "DAGABA",
     template: "%s | DAGABA",
   },
+  description:
+    "DAGABA, la nature aux bons soins des humains. DAGABA possède aujourd’hui plus de 2000 remèdes au niveau de son cabinet et les recherches continuent. La santé, la propriété, la paix, le développement ont toujours été l’objectif constamment visé par APROPHAM DAGABA.",
   keywords: ["Dagaba", "APROPHAM", "Mouleikafou"],
+  openGraph: {
+    title: "Dagaba | APROPHAM Mouleikafou",
+    description:
+      "DAGABA, la nature aux bons soins des humains. DAGABA possède aujourd’hui plus de 2000 remèdes au niveau de son cabinet et les recherches continuent. La santé, la propriété, la paix, le développement ont toujours été l’objectif constamment visé par APROPHAM DAGABA.",
+    url: "https://dagaba.net",
+    siteName: "Dagaba | APROPHAM Mouleikafou",
+    images: [
+      {
+        url: "https://dagaba.net/opengraph-image.jpg",
+        width: 1200,
+        height: 600,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
   robots: {
     index: true,
     follow: true,
@@ -22,13 +41,13 @@ export const metadata = {
       "max-snippet": -1,
     },
   },
-  icons: {
-    //shortcut: "/favicon.ico",
-  },
   verification: {
     //google: "eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw",
-    //yandex: "14d2e73487fa6c71",
+    other: {
+      me: ["dagabamouleikafou@gmail.com", "https://dagaba.net"],
+    },
   },
+  referrer: "origin-when-cross-origin",
 };
 
 export default function RootLayout({
